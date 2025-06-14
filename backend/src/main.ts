b6a8893a5a6ec6b3a65b198b4482c198044b6859
Tejanshu <tezz49332@gmail.com>
2025-06-14 22:03:03 +0530

    minor changes

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(app.get(ConfigService).getOrThrow('PORT'));
-}
-bootstrap();
